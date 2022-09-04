3d44175cf399d2e70236be46b72d2f87a153da1d
98-jeonghoon <bjh4395@naver.com>
2022-09-04 18:17:18 +0900

    Fix : 에러수정

---
+++ board/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from account.models import User
+
+
+class Board(models.Model): #발상의 전환
+    user_id = models.ForeignKey('account.User', on_delete=models.CASCADE, default=User.objects.first().pk)
+    title = models.CharField(default='', null=False, blank=False, max_length=50)
+    content = models.TextField(default='', null=False, blank=False)
+    write_time = models.DateField(auto_now_add=True)
+
+    def str(self):
+        return self.title
