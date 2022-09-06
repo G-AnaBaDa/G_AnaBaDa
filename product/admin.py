934a00e3cd9f8b5b66a90c6e0741b467506e96b6
98-jeonghoon <bjh4395@naver.com>
2022-09-06 17:11:30 +0900

    chore : 불필요한 코드 삭제 및 수정

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Product
-from django.contrib.auth.admin import UserAdmin
 
 class UserProduct(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'content', 'writer', 'write_time', 'location', 'hashtag', 'count')
