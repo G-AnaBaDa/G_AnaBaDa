72e83c4a80c030bf3fe61d93b9f565885e6d0f30
YongNyeo <won05121@naver.com>
2022-09-15 23:54:56 +0900

    FEAT : urls.py 수정

---
+++ product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from product.views import UploadProduct, productList, myPage
+from product import views as pviews
+
+urlpatterns= [
+    path('upload/', UploadProduct.as_view()),
+    path('list/', productList.as_view()),
+    path('list/<int:pk>', pviews.productDetail),
+    path('edit/<int:pk>/', pviews.editproduct),
+    path('delete/<int:pk>/', pviews.deleteproduct),
+    path('myPage/', myPage.as_view()),
+
+]
